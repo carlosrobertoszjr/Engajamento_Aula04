825a82fb0eda133b4e3b622daeaebcb124bf0cfe
Carlos Roberto <carlosrobertoszjr@gmail.com>
2020-08-26 00:17:46 +0000

    ..

--- main.cs
+++ main.cs
@@ -1,9 +1,4 @@
 using System;
-/*
-Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
-Para homens: (72.7*h) - 58
-Para mulheres: (62.1*h) - 44.7
-*/
 
 
 class MainClass {
@@ -11,30 +6,5 @@ class MainClass {
   public static double CalcPeso(double h, char sexo){
     double peso=-1;
 
-    if (sexo=='H')
-      peso = 72.7*h - 58;
-    else 
-      if (sexo=='M')
-        peso = 62.1*h - 44.7;
-        
-    return peso;
-  }
-
-  public static double CalcPesoSwitch(double h, char sexo){
-
-    switch (sexo){
-      case 'H':
-        return 72.7*h - 58;
-      case 'M':
-        return 62.1*h - 44.7;
-      default:
-        return -1;
-    }
-    
-  }
-
-  public static void Main (string[] args) {
-    Console.WriteLine(CalcPesoSwitch(1.7, 'X'));
-    
   }
 }
